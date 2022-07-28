ebfedbadc8a381637921e155ed2bac672df30575
XingyuZhang2018 <zhangxingyu@iphy.ac.cn>
2022-07-28 23:17:52 +0800

    lazy one

--- example/Kitaev.jl
+++ example/Kitaev.jl
@@ -8,6 +8,6 @@ using Zygote
 CUDA.allowscalar(false)
 
 Random.seed!(100)
-folder = "./example/Kitaev_1x2/"
-bulk, key = init_ipeps(K_J_Γ_Γ′(-1.0, 0.0, 0.0, 0.0), [1.0,1.0,1.0], 0.0;folder=folder, type = "_random", atype = Array, D=2, χ=20, tol=1e-10, maxiter=10, miniter=1)
-optimiseipeps(bulk, key; f_tol = 1e-10, opiter = 100, verbose = true)
+folder = "./example/Kitaev_1x2_new/"
+bulk, key = init_ipeps(K_J_Γ_Γ′(-1.0, 0.0, 0.0, 0.0), [1.0,1.0,1.0], 0.0;folder=folder, type = "_random", atype = CuArray, D=4, χ=50, tol=1e-10, maxiter=10, miniter=1)
+optimiseipeps(bulk, key; f_tol = 1e-10, opiter = 200, verbose = true)
