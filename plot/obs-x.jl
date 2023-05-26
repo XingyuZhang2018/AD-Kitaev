c9c22e78489fe4fd0b36283f32352150a06dc875
xyzhang <xyzhang@bright90.cm.cluster>
2023-05-26 09:31:33 +0800

    obs-x.jl

--- plot/obs-x.jl
+++ plot/obs-x.jl
@@ -3,23 +3,24 @@ using CUDA
 using FileIO
 using Printf: @sprintf
 using Random
+using LinearAlgebra: norm
 
 Random.seed!(100)
 folder  = "/data/xyzhang/ADBCVUMPS/"
 atype   = CuArray
-D, χ    = 4, 80
+D, χ    = 5, 100
 tol     = 1e-10
 maxiter = 10
 miniter = 1
-Ni, Nj  = 4, 4
-f       = 0.0:0.1:0.0
+Ni, Nj  = 1, 2
+f       = 0.01:0.01:1.0
 fdirect = [1.0, 1.0, 1.0]
 model   = K_J_Γ_Γ′(-1.0, -0.1, 0.3, -0.02)
 # 0.985263
 # 0.963424
 # 0.825221
-type = "_random"
-E, Ex, Ey, Ez, M = [], [], [], [], [], [], [], [], [], []
+type = "_NP"
+h, E, Ex, Ey, Ez, M = [], [], [], [], [], [], [], [], [], [], []
 for x in f
     @show x
     if x == 0.0
@@ -36,15 +37,39 @@ for x in f
     @show isdir(tfolder)
     if isdir(tfolder)
         y1, y2, y3, y4, y5 = observable(model, fdirect, x, "$(type)", folder, atype, D, χ, tol, maxiter, miniter, Ni, Nj)
-
-        global E  = [E;  y1]
-        global Ey = [Ey; y2]
-        global Ey = [Ey; y3]
-        global Ez = [Ez; y4]
-        global M  = [M;  y5]
+        push!(h,  x)
+        push!(E,  y1)
+        push!(Ex, y2)
+        push!(Ey, y3)
+        push!(Ez, y4)
+        push!(M,  y5)
     end
 end
 
-# print({})
-# for
-# @show collect(f) E
+print("{")
+for k in 1:length(h)
+    print("{$(h[k]),$(E[k])},")
+end
+print("};\n")
+
+print("{")
+for k in 1:length(h)
+    mag = [0,0,0]
+    for j in 1:Nj, i in 1:Ni
+        mag += sum(M[k][i,j,:])
+    end
+    mag = real(sum(mag/4/sqrt(3)))
+    print("{$(h[k]),$(mag)},")
+end
+print("};\n")
+
+print("{")
+for k in 1:length(h)
+    mag = [0,0,0]
+    for j in 1:Nj, i in 1:Ni
+        mag += sum(M[k][i,j,:])
+    end
+    mag = norm(mag/4)
+    print("{$(h[k]),$(mag)},")
+end
+print("}\n")
